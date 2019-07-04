91e62860faf4314d2c405a548f901602b41b4c0f
McPoet <queryfish@gmail.com>
2019-07-04 13:28:22 +0800

    .

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -94,9 +94,9 @@ class ZhipinPipeline(object):
 
 class DoubanBookPipeline(object):
     def open_spider(self, spider):
-        # self.file = open('output3.json', 'w')
+        self.file = open('output3.json', 'w')
     def close_spider(self, spider):
-        # self.file.close()
+        self.file.close()
     def process_item(self, item, spider):
         # client = pymongo.MongoClient(host="127.0.0.1", port=27017)
         # db = client['job']
