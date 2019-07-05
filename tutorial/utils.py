526e1f39faa236d4d4788744342458226b3c93f3
McPoet <queryfish@gmail.com>
2019-07-05 20:04:51 +0800

    .

--- tutorial/utils.py
+++ tutorial/utils.py
@@ -9,6 +9,8 @@ api_url = "https://dps.kdlapi.com/api/getdps/?orderid={}&num=1&pt=1&format=json&
 logger = logging.getLogger(__name__)
 cur_proxy = "202.183.32.182:80";
 reserved_proxy_count = 900;
+max_proxies = 2;
+switches = 0;
 
 def fetch_one_proxy2():
         return "202.183.32.182:80";
@@ -17,6 +19,7 @@ def fetch_one_proxy():
         """
             提取一个代理
         """
+        global switches, max_proxies
         fetch_url = api_url.format(orderid)
         r = requests.get(fetch_url)
         if r.status_code != 200:
@@ -26,7 +29,11 @@ def fetch_one_proxy():
 
         ips = content['data']['proxy_list']
         left = content['data']['order_left_count']
+        if(switches >= max_proxies):
+            logger.info("PROXY SWITCH TO 202.183.32.182:80");
+            return "202.183.32.182:80";
         if(left >= reserved_proxy_count):
             cur_proxy = ips[0];
+            switches = switches + 1;
         logger.info("PROXY SWITCH TO"+cur_proxy);
         return cur_proxy;
