9d9bddc89f6f2935625508c57f4c87959cd5c641
McPoet <queryfish@gmail.com>
2019-10-18 21:38:01 +0800

    concur is recreccted

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -18,15 +18,15 @@ RETRY_ENABLED = False
 # RETRY_TIMES = 5
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 5
+CONCURRENT_REQUESTS = 3
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #5秒的下载延时 / 每秒
-DOWNLOAD_DELAY = 0.2
+DOWNLOAD_DELAY = 0.4
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 5
+CONCURRENT_REQUESTS_PER_DOMAIN = 3
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
