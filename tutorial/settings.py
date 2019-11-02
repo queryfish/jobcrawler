7964878d805e50aebfcb6ba5a0276d41ab73b09a
McPoet <queryfish@gmail.com>
2019-11-02 23:31:53 +0800

    fixed delay and concur 4

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -20,13 +20,13 @@ RETRY_ENABLED = False
 DOWNLOAD_TIMEOUT = 30
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-# CONCURRENT_REQUESTS = 2
+CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #5秒的下载延时 / 每秒
-DOWNLOAD_DELAY = 0.2
+DOWNLOAD_DELAY = 0.3
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 5
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -79,14 +79,14 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-AUTOTHROTTLE_START_DELAY = 1
-# The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 5
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 5.0
+# AUTOTHROTTLE_ENABLED = True
+# # The initial download delay
+# AUTOTHROTTLE_START_DELAY = 0.1
+# # The maximum download delay to be set in case of high latencies
+# AUTOTHROTTLE_MAX_DELAY = 5
+# # The average number of requests Scrapy should be sending in parallel to
+# # each remote server
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 5.0
 # Enable showing throttling stats for every response received:
 AUTOTHROTTLE_DEBUG = True
 
