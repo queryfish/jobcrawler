de07896302aa98cd573d9c92c295908ae48d0795
McPoet <queryfish@gmail.com>
2019-11-02 20:03:25 +0800

    parse and refetch from mongo

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -26,7 +26,7 @@ DOWNLOAD_TIMEOUT = 30
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #5秒的下载延时 / 每秒
-DOWNLOAD_DELAY = 1.0
+DOWNLOAD_DELAY = 0.2
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 5
 #CONCURRENT_REQUESTS_PER_IP = 16
