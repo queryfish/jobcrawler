39cb4edcae9160ffaaa011f652136c3fac21cc7e
McPoet <queryfish@gmail.com>
2019-11-06 15:28:33 +0800

    .

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,7 +11,7 @@ LOG_LEVEL = 'INFO'
 LOG_FILE = './douban_book.log'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
-HTTPERROR_ALLOWED_CODES = [403,404]
+HTTPERROR_ALLOWED_CODES = [403,404] 
 # Obey robots.txt rules
 #ROBOTSTXT_OBEY = True
 RETRY_ENABLED = False
