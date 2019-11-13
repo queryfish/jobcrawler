3db4431964f64a0ee6b7ffb2c2aa591e2c6e8442
McPoet <queryfish@gmail.com>
2019-11-13 23:09:23 +0800

    .

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,25 +11,25 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 # LOG_FILE = './proxy_douban_book.log'
 # # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
-# HTTPERROR_ALLOWED_CODES = [403,404]
+HTTPERROR_ALLOWED_CODES = [403,404]
 # # Obey robots.txt rules
 # #ROBOTSTXT_OBEY = True
-# RETRY_ENABLED = False
+RETRY_ENABLED = False
 # #RETRY_TIMES = 1
 #
-# DOWNLOAD_TIMEOUT = 7.5
-# DUPEFILTER_DEBUG = True
+DOWNLOAD_TIMEOUT = 7.5
+DUPEFILTER_DEBUG = True
 #
-# # LOGSTATS_INTERVAL = 300.0
+LOGSTATS_INTERVAL = 60.0
 # # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 2
+CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #5秒的下载延时 / 每秒
 # 2.0/CONCURRENT_REQUESTS
-# DOWNLOAD_DELAY = .1
+DOWNLOAD_DELAY = .1
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 5
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -82,16 +82,16 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-# AUTOTHROTTLE_ENABLED = False
-# # The initial download delay
-# AUTOTHROTTLE_START_DELAY = 0.1
-# # The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 5
-# # The average number of requests Scrapy should be sending in parallel to
-# # each remote server
-# AUTOTHROTTLE_TARGET_CONCURRENCY = 5.0
-# # Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = True
+AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+AUTOTHROTTLE_START_DELAY = 0.1
+# The maximum download delay to be set in case of high latencies
+AUTOTHROTTLE_MAX_DELAY = 1
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+AUTOTHROTTLE_TARGET_CONCURRENCY = 5.0
+# Enable showing throttling stats for every response received:
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
