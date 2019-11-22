198018fb635a77a887fee1e2929163ee4946e77e
McPoet <queryfish@gmail.com>
2019-11-23 02:50:45 +0800

    .

--- scrapy_daemon.sh
+++ scrapy_daemon.sh
@@ -1,5 +1,7 @@
-until `scrapy crawl dbbCrawler --logfile ./logs/rulespider201911.log --loglevel INFO`;
+until `scrapy crawl $1 --logfile ./logs/{%1}$(date +%Y%m%d%H).log --loglevel INFO`;
 do
     echo "$(date):Server 'myserver' crashed with exit code $?.  Respawning.." >&2
     sleep 60
 done
+
+# echo $1 $(date +%Y%m%d_%H)
