781311b496259cf024a8c67aa3faa4f46761fbe4
McPoet <queryfish@gmail.com>
2019-11-23 02:54:02 +0800

    change log format correspond to date in the daemon

--- scrapy_daemon.sh
+++ scrapy_daemon.sh
@@ -1,4 +1,4 @@
-until `scrapy crawl $1 --logfile ./logs/{%1}$(date +%Y%m%d%H).log --loglevel INFO`;
+until `scrapy crawl $1 --logfile ./logs/$1$(date +%Y%m%d%H).log --loglevel INFO`;
 do
     echo "$(date):Server 'myserver' crashed with exit code $?.  Respawning.." >&2
     sleep 60
